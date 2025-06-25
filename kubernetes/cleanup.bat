a9bcac1e3be0005127608655f054971e6022863d
Daniel <daniel.ctin.pavelescu@gmail.com>
2025-06-25 14:14:48 +0300

    feat: Update task status to 'done' in tasks.json; modify cleanup script to correct resource deletion order

--- kubernetes/cleanup.bat
+++ kubernetes/cleanup.bat
@@ -4,8 +4,7 @@ REM Cleanup Kubernetes deployment (Windows)
 echo 🧹 Cleaning up Resource Sizing Service deployment...
 
 REM Delete all resources in reverse order
-kubectl delete -f kubernetes/app/06-hpa.yaml
-kubectl delete -f kubernetes/app/05-servicemonitor.yaml
+kubectl delete -f kubernetes/app/05-hpa.yaml
 kubectl delete -f kubernetes/app/04-service.yaml
 kubectl delete -f kubernetes/app/03-deployment.yaml
 kubectl delete -f kubernetes/app/02-configmap.yaml
